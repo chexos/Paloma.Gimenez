bfdad3e4325e457bb56620b883de753920716470
chexos <sebastggl@gmail.com>
2024-03-11 13:22:15 -0500

    Menu de navegación móvil actualizado, se agregó la página producto

--- js/header.js
+++ js/header.js
@@ -1,10 +1,14 @@
 let menu = document.getElementById("menu");
 let menuMovil = document.getElementById("menuMovil");
+let menumovil = document.getElementsByClassName("menu-movil");
 function moverMenu(e) {
-    e.preventDefault();
     if (!menuMovil.classList.contains("visibility"))
         menuMovil.classList.add("visibility");
     else
         menuMovil.classList.remove("visibility");
 }
+menumovil[0].addEventListener("click", moverMenu);
+menumovil[1].addEventListener("click", moverMenu);
+menumovil[2].addEventListener("click", moverMenu);
+menumovil[3].addEventListener("click", moverMenu);
 menu.addEventListener("click", moverMenu);
