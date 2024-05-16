65380012591e1e5ee11e18dd4f4ca05ee67fb4df
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-05-16 13:16:08 -0500

    Moví el breakpoint de la cabecera que estaba en el archivo index.css a header.css

--- js/header.js
+++ js/header.js
@@ -3,13 +3,16 @@ let menu = document.getElementById("menu");
 let menuMovil = document.getElementById("menuMovil");
 let menumovil = document.getElementsByClassName("menu-movil");
 function moverMenu(e) {
-    if (!menuMovil.classList.contains("visibility"))
-        menuMovil.classList.add("visibility");
+    if (!menuMovil.classList.contains("visibilidad"))
+        menuMovil.classList.add("visibilidad");
     else
-        menuMovil.classList.remove("visibility");
+        menuMovil.classList.remove("visibilidad");
 }
 menumovil[0].addEventListener("click", moverMenu);
 menumovil[1].addEventListener("click", moverMenu);
 menumovil[2].addEventListener("click", moverMenu);
 menumovil[3].addEventListener("click", moverMenu);
 menu.addEventListener("click", moverMenu);
+function removerTransicion() {
+
+}
