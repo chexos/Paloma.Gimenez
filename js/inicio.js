4209ab4954cb0a4c2ae7314303a7c29431f5f369
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-05-28 00:51:24 -0500

    Necesita verse el tema del espacio en blanco antes o después del salto de línea

--- js/inicio.js
+++ js/inicio.js
@@ -45,10 +45,14 @@ spanAncho.style.position = "absolute";
 spanAncho.style.maxWidth = "100%";
 spanAncho.style.fontFamily = "Hurme Semi Bold";
 spanAncho.style.color = "Gray"; 
+spanAncho.style.whiteSpace = "pre-wrap";
 //span.style.display = "none";
 articuloTexto.appendChild(spanAncho);
 function ultimoCaracter() {
     var ultimo = mensaje.value.lastIndexOf(" ");
+    console.log(mensaje.value);
+    console.log(mensaje.value.length);
+    console.log(ultimo);
     return ultimo + 1;
 }
 function removerDobleEspacioBlanco() {
@@ -59,25 +63,47 @@ function removerDobleEspacioBlanco() {
 let altoDesplazamiento = mensaje.scrollHeight;
 let altoMensaje = mensaje.offsetHeight;
 let anchoDesplazamiento = mensaje.scrollWidth;
-let anchoMensaje = mensaje.clientWidth;
+let anchoMensaje;
+function actualizarAnchoMensaje() {
+    anchoMensaje = mensaje.clientWidth
+}
+mensaje.addEventListener("loaded", actualizarAnchoMensaje());
+window.addEventListener("resize", actualizarAnchoMensaje());
 var m2;
 var m;
 function generarSalto() {
     f = mensaje.value.split("\n").length;
-    spanAncho.innerText = mensaje.value;
+    spanAncho.textContent = mensaje.value;
     console.log(spanAncho.offsetWidth);
     console.log(anchoMensaje);
     revisarAnchoAlto();
 }
 function revisarAnchoAlto() {
-    if (spanAncho.offsetWidth >= anchoMensaje) {
+    if (spanAncho.offsetWidth > anchoMensaje) {
         console.log(spanAncho.offsetWidth);
         console.log(anchoMensaje);
         m2 = mensaje.value.slice(ultimoCaracter());
+        console.log(m2);
         m = mensaje.value.slice(mensaje.length, ultimoCaracter());
+        if (m2 == "") {
+            console.l
+            var u = mensaje.value.lastIndexOf(" ", ultimoCaracter() - 2);
+            console.log(u);
+            m2 = mensaje.value.slice(u);
+            console.log(m2);
+            m2 = mensaje.value.slice(u);
+            console.log(m2);
+            m = mensaje.value.slice(mensaje.length, u);
+        }
+        console.log(m2);
+        console.log(m);
         console.log(mensaje.value);
         mensaje.value = m + "\n" + m2;
         console.log(mensaje.value);
+        spanAncho.textContent = mensaje.value;
+        //let textoConEspacio = spanAncho.textContent.replace("/\n/g", "<br> ");
+        spanAncho.textContent = textoConEspacio;
+        console.log(spanAncho.textContent);
     }
     if (altoDesplazamiento > altoMensaje) {
         altoMensaje = mensaje.offsetHeight;
@@ -87,35 +113,60 @@ function revisarAnchoAlto() {
 function tamanioLetra() {
     spanAncho.style.fontSize = window.getComputedStyle(document.getElementById("mensaje"), null).getPropertyValue('font-size');
 }
+let ajuste;
+function esperarAjuste() {
+    clearTimeout(ajuste);
+    ajuste = setTimeout(function() {
+        anchoMensaje = mensaje.offsetWidth;
+    }, 100);
+}
 let dividirMensaje;
-function cambiarResolucion(e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
+let navegador = 0;
+function cambiarResolucion() {
     tamanioLetra();
-    anchoMensaje = mensaje.clientWidth;
-    console.log(dividirMensaje);
-    console.log(mensaje.value.split(" "));
-    dividirMensaje = mensaje.value.split(" ");
-    console.log(mensaje.value);
-    console.log(mensaje.value.split(" "));
-    ajustarValor();
-    console.log(dividirMensaje.length);
-    for (var i = 0; i < dividirMensaje.length; i++) {
-        console.log(i);
-        console.log(dividirMensaje[i]);
-        if (!dividirMensaje[i].startsWith("\n") && i != dividirMensaje.length - 1) {
-            console.log("sin salto");
-            mensaje.value += dividirMensaje[i] + " ";
-        } else if (i == dividirMensaje.length - 1) {
-            console.log("última cadena");
-            mensaje.value += dividirMensaje[i];
-        } else if (dividirMensaje[i].startsWith("\n")) {
-            console.log("con salto");
-            mensaje.value += dividirMensaje[i].slice(1) + " ";
+    console.log(window.getComputedStyle(document.getElementById("mensaje"), null).getPropertyValue('offsetWidth'));
+    if (navigator.userAgent.search("Chrome") > 0) {
+        navegador++;
+    }
+    if (navegador == 2) {
+        navegador = 0;
+        tamanioLetra();
+        anchoMensaje = mensaje.clientWidth;
+        console.log(dividirMensaje);
+        console.log(mensaje.value.split(" "));
+        dividirMensaje = mensaje.value.split(" ");
+        console.log(mensaje.value);
+        console.log(mensaje.value.split(" "));
+        ajustarValor();
+        console.log(dividirMensaje.length);
+        console.log(anchoMensaje);
+        console.log(mensaje.offsetWidth);
+        anchoMensaje = mensaje.offsetWidth;
+        console.log(anchoMensaje);
+        actualizarAnchoMensaje();
+        console.log(anchoMensaje);
+        console.log(mensaje.scrollWidth);
+        for (var i = 0; i < dividirMensaje.length; i++) {
+            spanAncho.textContent = mensaje.value;
+            console.log(spanAncho.offsetWidth);
+            console.log(i);
+            console.log(dividirMensaje[i]);
+            if (!dividirMensaje[i].startsWith("\n") && i != dividirMensaje.length - 1) {
+                console.log("sin salto");
+                mensaje.value += dividirMensaje[i] + " ";
+            } else if (i == dividirMensaje.length - 1) {
+                console.log("última cadena");
+                mensaje.value += dividirMensaje[i];
+            } else if (dividirMensaje[i].startsWith("\n")) {
+                console.log("con salto");
+                //mensaje.value += dividirMensaje[i] + " ";
+                mensaje.value += dividirMensaje[i].slice(1) + " ";
+            } else {
+    
+            }
+            generarSalto();
+            removerDobleEspacioBlanco();
         }
-        spanAncho.innerText = mensaje.value;
-        revisarAnchoAlto();
-        removerDobleEspacioBlanco();
     }
 }
 function entradaFilas() {
