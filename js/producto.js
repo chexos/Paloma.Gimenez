128530e6eae53a91aa48f8a79da1f93c347ef947
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-07-26 16:50:32 -0500

    Actualización al dinanismo de producto en la activación y desactivación del resaltado

--- js/producto.js
+++ js/producto.js
@@ -11,77 +11,77 @@ function resaltarUno() {
 }
 sesion[0].addEventListener("click", resaltarUno);
 function resaltarDos() {
-    eliminarResaltado();
+    eliminarResaltado(1);
     sesion[1].classList.toggle("resaltar");
 }
 sesion[1].addEventListener("click", resaltarDos);
 function resaltarTres() {
-    eliminarResaltado();
+    eliminarResaltado(2);
     sesion[2].classList.toggle("resaltar");
 }
 sesion[2].addEventListener("click", resaltarTres);
 function resaltarCuatro() {
-    eliminarResaltado();
+    eliminarResaltado(3);
     sesion[3].classList.toggle("resaltar");
 }
 sesion[3].addEventListener("click", resaltarCuatro);
 function resaltarCinco() {
-    eliminarResaltado();
+    eliminarResaltado(4);
     sesion[4].classList.toggle("resaltar");
 }
 sesion[4].addEventListener("click", resaltarCinco);
 function resaltarSeis() {
-    eliminarResaltado();
+    eliminarResaltado(5);
     sesion[5].classList.toggle("resaltar");
 }
 sesion[5].addEventListener("click", resaltarSeis);
 function resaltarSiete() {
-    eliminarResaltado();
+    eliminarResaltado(6);
     sesion[6].classList.toggle("resaltar");
 }
 sesion[6].addEventListener("click", resaltarSiete);
 function resaltarOcho() {
-    eliminarResaltado();
+    eliminarResaltado(7);
     sesion[7].classList.toggle("resaltar");
 }
 sesion[7].addEventListener("click", resaltarOcho);
 function resaltarNueve() {
-    eliminarResaltado();
+    eliminarResaltado(8);
     sesion[8].classList.toggle("resaltar");
 }
 sesion[8].addEventListener("click", resaltarNueve);
 function resaltarDiez() {
-    eliminarResaltado();
+    eliminarResaltado(9);
     sesion[9].classList.toggle("resaltar");
 }
 sesion[9].addEventListener("click", resaltarDiez);
 function resaltarOnce() {
-    eliminarResaltado();
+    eliminarResaltado(10);
     sesion[10].classList.toggle("resaltar");
 }
 sesion[10].addEventListener("click", resaltarOnce);
 function resaltarDoce() {
-    eliminarResaltado();
+    eliminarResaltado(11);
     sesion[11].classList.toggle("resaltar");
 }
 sesion[11].addEventListener("click", resaltarDoce);
 function resaltarTrece() {
-    eliminarResaltado();
+    eliminarResaltado(12);
     sesion[12].classList.toggle("resaltar");
 }
 sesion[12].addEventListener("click", resaltarTrece);
 function resaltarCatorce() {
-    eliminarResaltado();
+    eliminarResaltado(13);
     sesion[13].classList.toggle("resaltar");
 }
 sesion[13].addEventListener("click", resaltarCatorce);
 function resaltarQuince() {
-    eliminarResaltado();
+    eliminarResaltado(14);
     sesion[14].classList.toggle("resaltar");
 }
 sesion[14].addEventListener("click", resaltarQuince);
 function resaltarDeciseis() {
-    eliminarResaltado();
+    eliminarResaltado(15);
     sesion[15].classList.toggle("resaltar");
 }
 sesion[15].addEventListener("click", resaltarDeciseis);/*
