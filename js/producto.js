7b141987874bd209cd314c5053504dae12cf1c10
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-08-08 14:20:22 -0500

    Se cambió de nombre a la variable sesión a figura sesión

--- js/producto.js
+++ js/producto.js
@@ -1,28 +1,28 @@
 let ampliar = document.getElementById("ampliar");
 let imagenAmpliada = document.getElementById("imagenAmpliada");
-let sesion = document.getElementsByClassName("sesion");
+let figuraSesion = document.getElementsByClassName("figura-sesion");
 let estiloAmpliar = window.getComputedStyle(ampliar);
 let displayAmpliar;
 function eliminarResaltado(a) {
-    sesion[a].classList.remove("resaltar");
+    figuraSesion[a].classList.remove("resaltar");
 }
 function resaltarUno(a) {
     eliminarResaltado(a);
-    sesion[a].classList.toggle("resaltar");
+    figuraSesion[a].classList.toggle("resaltar");
 }
 function ampliarImagen(a) {
     displayAmpliar = estiloAmpliar.display;
     console.log(displayAmpliar);
     if (displayAmpliar != "flex") {
         ampliar.style.display = "flex";
-        imagenAmpliada.src = sesion[a].src;
+        imagenAmpliada.src = figuraSesion[a].src;
     } else {
         ampliar.style.display = "none";
     }
 }
-for (let i = 0; i < sesion.length; i++) {
-    sesion[i].addEventListener("touchstart", () => resaltarUno(i));
-    sesion[i].addEventListener("touchend", () => eliminarResaltado(i));
-    sesion[i].addEventListener("click", () => ampliarImagen(i));
+for (let i = 0; i < figuraSesion.length; i++) {
+    figuraSesion[i].addEventListener("touchstart", () => resaltarUno(i));
+    figuraSesion[i].addEventListener("touchend", () => eliminarResaltado(i));
+    figuraSesion[i].addEventListener("click", () => ampliarImagen(i));
 }
 ampliar.addEventListener("click", () => ampliarImagen(0));
