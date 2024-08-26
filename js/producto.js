047ad9e57ced0c14061a899da08c19962c6e7a49
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-08-26 13:14:49 -0500

    Actualizaciones a los nombres del arreglo imagen en el dinanismo de producto

--- js/producto.js
+++ js/producto.js
@@ -19,19 +19,18 @@ imagen[4] = "Tamaño-Producto-603x670-5.png";
 imagen[5] = "Tamaño-Producto-603x670-6.png";
 imagen[6] = "Tamaño-Producto-603x670-7.png";
 imagen[7] = "Tamaño-Producto-603x670-8.png";
-imagen[8] = "Tamaño-Producto-603x670-9.png";
+imagen[8] = "Tamaño-Producto-603x670-9.gif";
 imagen[9] = "Tamaño-Producto-603x670-10.png";
 imagen[10] = "Tamaño-Producto-603x670-11.png";
-imagen[11] = "Tamaño-Producto-603x670-12.png";
-imagen[12] = "Tamaño-Producto-603x670-13.png";
+imagen[11] = "Tamaño-Producto-603x670-12.gif";
+imagen[12] = "Tamaño-Producto-603x670-13.gif";
 imagen[13] = "Tamaño-Producto-603x670-14.png";
-imagen[14] = "Tamaño-Producto-603x670-15.png";
+imagen[14] = "Tamaño-Producto-603x670-15.gif";
 imagen[15] = "Tamaño-Producto-603x670-16.png";
-imagen[16] = "Tamaño---Producto---603x670---gif---1.gif";
-imagen[17] = "Tamaño---Producto---603x670---gif---2.gif";
-imagen[18] = "Tamaño---Producto---603x670---gif---3.gif";
-imagen[19] = "Tamaño---Producto---603x670---gif---4.gif";
-imagen[20] = "Tamaño---Producto---603x670---gif---5.gif";
+imagen[16] = "Tamaño-Producto-603x670-17.gif";
+imagen[17] = "Tamaño-Producto-603x670-18.png";
+imagen[18] = "Tamaño-Producto-603x670-19.png";
+imagen[19] = "Tamaño-Producto-603x670-20.png";
 function intercambiarImagen(a) {
     estado = true;
     if (a == 1) {
