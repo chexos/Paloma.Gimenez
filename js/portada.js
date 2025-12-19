7ce11e2066333ae2842adfbde51984db86ddad53
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-19 14:00:53 -0500

    Se creó la variable p y se inicializó con el valor cero en la función cambiar portada del archivo portada.js

--- js/portada.js
+++ js/portada.js
@@ -1,4 +1,5 @@
 function cambiarPortada() {
+    let p = 0;
     let botonesCarrusel = document.getElementsByClassName("btn-carrusel");
 }
 document.addEventListener("DOMContentLoaded", function() {
