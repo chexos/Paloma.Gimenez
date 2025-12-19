65f526f817c6ba499bb809c75908be204569dea2
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-19 14:08:20 -0500

    En la función verificar portada se puso un switch de la variable p.

--- js/portada.js
+++ js/portada.js
@@ -3,7 +3,9 @@ function portada() {
     let cp = -1;
     let botonesCarrusel = document.getElementsByClassName("btn-carrusel");
     function verificarPortada() {
-        
+        switch (p) {
+            
+        }
     }
     function cambiarPortada() {
     }
