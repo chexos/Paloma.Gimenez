263db4eaf5895795fd2ba9a86eb264a05156944f
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-19 14:02:39 -0500

    Se movió la función cambiar portada dentro de la función portada en el archivo portda.js.

--- js/portada.js
+++ js/portada.js
@@ -1,9 +1,8 @@
 function portada() {
-    
-}
-function cambiarPortada() {
-    let p = 0;
-    let botonesCarrusel = document.getElementsByClassName("btn-carrusel");
+    function cambiarPortada() {
+        let p = 0;
+        let botonesCarrusel = document.getElementsByClassName("btn-carrusel");
+    }
 }
 document.addEventListener("DOMContentLoaded", function() {
 
