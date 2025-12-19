952b0a571757a7a03f7298b69c37320ab225277e
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-19 14:59:56 -0500

    Se movió la variable cp a antes de la función cambiar circulo y se creó la condificonal si i es igual a la variable cp en la función cambiar circulo para que ese botón carrusel agregue la clase botón seleccionado.

--- js/portada.js
+++ js/portada.js
@@ -1,12 +1,14 @@
 function portada() {
     let botonesCarrusel = document.getElementsByClassName("btn-carrusel");
+    let cp = -1;
     function cambiarCirculo() {
         for (var i = 0; i < botonesCarrusel.length; i++) {
-
+            if (i == cp) {
+                botonesCarrusel[i].classList.add("btn-seleccionado");
+            }
         }
     }
     let p = 0;
-    let cp = -1;
     function verificarPortada() {
         switch (p) {
             case 0:
