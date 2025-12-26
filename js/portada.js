b24dbe21fd260c4f8aa177e04224b75c3428c686
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-26 13:05:58 -0500

    Se hicieron los cambios para que funcione correctamente el javascript portada.

--- js/portada.js
+++ js/portada.js
@@ -26,98 +26,98 @@ function portada() {
                 circulos.classList.add("trasladar-1300");
                 break;
             case 1:
-                cp = 1;
+                cp = 0;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("primera-imagen");
                 circulos.classList.add("trasladar-100");
                 break;
             case 2:
-                cp = 2;
+                cp = 1;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("segunda-imagen");
                 circulos.classList.add("trasladar-200");
                 break;
             case 3:
-                cp = 3;
+                cp = 2;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("tercera-imagen");
                 circulos.classList.add("trasladar-300");
                 break;
             case 4:
-                cp = 4;
+                cp = 3;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("cuarta-imagen");
                 circulos.classList.add("trasladar-400");
                 break;
             case 5:
-                cp = 5;
+                cp = 4;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("quinta-imagen");
                 circulos.classList.add("trasladar-500");
                 break;
             case 6:
-                cp = 6;
+                cp = 5;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("sexta-imagen");
                 circulos.classList.add("trasladar-600");
                 break;
             case 7:
-                cp = 7;
+                cp = 6;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("septima-imagen");
                 circulos.classList.add("trasladar-700");
                 break;
             case 8:
-                cp = 8;
+                cp = 7;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("octava-imagen");
                 circulos.classList.add("trasladar-800");
                 break;
             case 9:
-                cp = 9;
+                cp = 8;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("novena-imagen");
                 circulos.classList.add("trasladar-900");
                 break;
             case 10:
-                cp = 10;
+                cp = 9;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("decima-imagen");
                 circulos.classList.add("trasladar-1000");
                 break;
             case 11:
-                cp = 11;
+                cp = 10;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("onceava-imagen");
                 circulos.classList.add("trasladar-1100");
                 break;
             case 12:
-                cp = 12;
+                cp = 11;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("doceava-imagen");
                 circulos.classList.add("trasladar-1200");
                 break;
             case 13:
-                cp = 13;
+                cp = 12;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("treceava-imagen");
                 circulos.classList.add("trasladar-1300");
                 break;
             case 14:
-                cp = 14;
+                cp = 13;
                 cambiarCirculo();
                 removerClasesPortada();
                 carrusel.classList.add("catorceava-imagen");
@@ -128,12 +128,12 @@ function portada() {
         }
     }
     function primerCirculo() {
-        cp = 0;
+        p = 1;
         verificarPortada();
     }
     botonesCarrusel[0].addEventListener("click", primerCirculo);
     function SegundoCirculo() {
-        cp = 1;
+        p = 2;
         verificarPortada();
     }
     botonesCarrusel[1].addEventListener("click", SegundoCirculo);
